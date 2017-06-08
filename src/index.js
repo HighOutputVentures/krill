552af71dc79712949ff0c768f705357889e2cc81
Arjay A. Sitoy <sitoy.arjay@gmail.com>
2017-06-08 15:45:04 +0800

    refactor(index): load all necessary files on start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import _ from 'lodash';
 import path from 'path';
 import Promise from 'bluebird';
+import router from './lib/router';
 import utilities from './lib/utilities';
 
 global.Util = utilities;
@@ -11,11 +12,21 @@ global.Module = {};
 
 export default {
   async start() {
+    /* load policies and resources to the global object */
+    Util.require(path.join(process.cwd(), 'policies'), 'Policies');
+    Util.require(path.join(process.cwd(), 'resources'), 'Resources');
+
+    /* require all the config files */
+    const routes = require(path.join(process.cwd(), 'config/routes')).default;
+    const middlewares = require(path.join(process.cwd(), 'config/middlewares')).default;
     const bootloaders = require(path.join(process.cwd(), 'config/bootloaders')).default;
     const adapters = require(path.join(process.cwd(), 'config/adapters')).default;
 
     /* load bootloaders */
     _.each(bootloaders, bootloader => bootloader());
+
+    // routes, policies, schema, middlewares
+    router(routes, global.Policies, global.Resources);
 
     /* start adapters */
     await Promise.all(_.map(adapters, async (adapter) => {
