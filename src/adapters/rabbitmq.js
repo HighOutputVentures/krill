9b36ea7f79ba078ab6be36f0ed1287ee405b231e
Arjay A. Sitoy <sitoy.arjay@gmail.com>
2017-06-12 13:23:19 +0800

    chore(npm): bump to v0.1.5

--- src/adapters/rabbitmq.js
+++ src/adapters/rabbitmq.js
@@ -35,19 +35,22 @@ export class AMQP {
       compose(this.middlewares.slice(0).concat(resource)) :
       compose(this.middlewares.slice(0).concat([resource]));
 
-    const worker = await this.arque.createWorker({ job: name, concurrency: 500 }, async ({ body }) => {
-      try {
-        const ctx = { request: {}, response: {} };
-        ctx.route = name;
-        ctx.request.body = body;
-        await stack(ctx);
+    const worker = await this.arque.createWorker(
+      { job: name, concurrency: 500 },
+      async ({ body }) => {
+        try {
+          const ctx = { request: {}, response: {} };
+          ctx.route = name;
+          ctx.request.body = body;
+          await stack(ctx);
 
-        return { body: ctx.response.body, code: 'success' };
-      } catch (err) {
-        logger(`route: ${name}, request: ${JSON.stringify(body, null, 2)}, error: ${err.message}`);
-        return { code: 'invalid_request', body: err.message };
-      }
-    });
+          return { body: ctx.response.body, code: 'success' };
+        } catch (err) {
+          logger(`route: ${name}, request: ${JSON.stringify(body, null, 2)}, error: ${err.message}`);
+          return { code: 'invalid_request', body: err.message };
+        }
+      },
+    );
 
     this.workers.push(worker);
   }
