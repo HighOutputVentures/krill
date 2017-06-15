d635f4e3a343d93d603ec0fc75f28ecb0a1ff537
Arjay A. Sitoy <sitoy.arjay@gmail.com>
2017-06-15 16:51:59 +0800

    feat(amplitude): catch request errors and log it

--- src/adapters/amplitude.js
+++ src/adapters/amplitude.js
@@ -1,15 +1,24 @@
+/* globals Adapter */
 import request from 'request-promise';
-import qs from 'querystring';
+import debug from 'debug';
 
 const { AMPLITUDE_KEY } = process.env;
+const logger = debug('amplitude');
 
 export default {
   async start() {
     Adapter.Amplitude = async ({ event }) => {
-      await request('https://api.amplitude.com')
-        .post('/httpapi')
-        .set('content-type', 'application/x-www-form-urlencoded')
-        .data(`api_key=${AMPLITUDE_KEY}&event=${JSON.stringify(event)}`);
+      try {
+        await request('https://api.amplitude.com')
+          .post('/httpapi')
+          .set('content-type', 'application/x-www-form-urlencoded')
+          .data(`api_key=${AMPLITUDE_KEY}&event=${JSON.stringify(event)}`);
+      } catch (err) {
+        logger(`event: ${event}, ${err.message}`);
+        const error = new Error(err.message);
+        error.name = 'AmplitudeAdapterError';
+        throw error;
+      }
     };
   },
 
