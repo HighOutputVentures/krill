cf68aea740342b9264850fc9c8b98913fa97afe5
Arjay A. Sitoy <sitoy.arjay@gmail.com>
2017-07-10 14:25:44 +0800

    test(mailer): add tests for arrays with length 1

--- test/lib/mail.queue.test.js
+++ test/lib/mail.queue.test.js
@@ -22,6 +22,26 @@ test.cb('Array size: 3, prefetch: 5', t => {
   queue.dispatch();
 });
 
+test.cb('Array size: 1, prefetch: 5', t => {
+  const queue = new MailQueue({ emails: [
+    { from: 'Identifi <noreply@Identifi.com>', to: 'arjay@highoutput.com', content: 'sample email' },
+  ], prefetch: 5 });
+
+  queue.on('dispatch', (data) => { t.is(data.dispatched.length, 1); });
+  queue.on('done', t.end);
+  queue.dispatch();
+});
+
+test.cb('Array size: 1, prefetch: 1', t => {
+  const queue = new MailQueue({ emails: [
+    { from: 'Identifi <noreply@Identifi.com>', to: 'arjay@highoutput.com', content: 'sample email' },
+  ], prefetch: 5 });
+
+  queue.on('dispatch', (data) => { t.is(data.dispatched.length, 1); });
+  queue.on('done', t.end);
+  queue.dispatch();
+});
+
 test.cb('Array size: 0, prefetch: 5', t => {
   const queue = new MailQueue({ emails: [], prefetch: 5 });
 
