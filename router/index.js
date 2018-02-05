1fb6b413e1cfb4d1f9f83d433b493874b734a1cc
Arjay Sitoy <sitoy.arjay@gmail.com>
2018-02-06 00:26:23 +0800

    [WIP!] feat(route): revamp router mechanism

--- router/index.js
+++ router/index.js
@@ -1,6 +1,47 @@
 const _ = require('lodash');
+const each = require('lodash/each');
+const get = require('lodash/get');
 const Ajv = require('ajv');
 const loadSchema = require('../utilities').loadSchema;
+
+/*
+ * TODO: routing mechanism components 
+ * 1. route stack, which contains all the stacked polices and resource
+ * 2. route resolver, which maps the stack with the http api 
+ * 3. router, loads mapped routes according to route type
+ */
+
+
+class RouteStack {
+  constructor(options) {
+    this.stack = [];
+
+    this.api = options.api;
+    this.type = options.type;
+    this.policies = options.policies;
+    this.resources = options.resources;
+  }
+
+  stackPolicies(policyConfig = []) {
+    each(policyConfig, policyName => {
+      if(!this.policies[policyName]) {
+        throw new Error('Policy not found');
+      }
+
+      stack.push(this.policies[policyName]);
+    });
+  }
+
+  stackResource(resource) {
+    const resourceObject = get(this.resources, resource);
+
+    if (!resourceObject) {
+      throw new Error(`${resource} resource not found`);
+    }
+
+    this.stack.push(resourceObject);
+  }
+}
 
 module.exports = function (routes, resources, policies) {
   const ajv = new Ajv();
@@ -14,36 +55,6 @@ module.exports = function (routes, resources, policies) {
       const error = new Error(`${resource} resource not found`);
       error.name = 'RouterError';
       throw error;
-    }
-
-    /* Stack schema validator */
-    if (schema) {
-      stack.push(async (ctx, next) => {
-        const {body, headers} = schema;
-
-        let valid;
-
-        if (!body && !headers) {
-          await next();
-          return;
-        }
-
-        if (body) {
-          valid = ajv.validate(body, ctx.request.body);
-        }
-
-        if (headers) {
-          valid = ajv.validate(headers, ctx.request.headers);
-        }
-
-        if (!valid) {
-          const error = new Error(ajv.errorsText());
-          error.name = 'AjvError';
-          throw error;
-        }
-
-        await next();
-      });
     }
 
     /* Stack policies */
