9ba7c8feab128fae3cf8adcfb2a08e471be745a0
Arjay Sitoy <sitoy.arjay@gmail.com>
2018-03-01 14:39:09 +0800

    refactor(tools): deprecate mailgun and ajv

--- utilities.js
+++ utilities.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const merge = require('lodash/merge');
 const each = require('lodash/each');
-const Ajv = require('ajv');
 
 module.exports = {};
 
@@ -18,24 +17,4 @@ module.exports.load = function (dir) {
   });
 
   return object;
-};
-
-module.exports.validate = function (schema, json) {
-  const ajv = new Ajv();
-  const valid = ajv.validate(
-    require(path.join(process.cwd(), 'schema', schema)), json,
-  );
-
-  if (!valid) {
-    throw new Error(ajv.errorsText());
-  }
-};
-
-module.exports.loadSchema = function (ajv, dir) {
-  const schemas = fs.existsSync(path.join(process.cwd(), dir)) ?
-    fs.readdirSync(path.join(process.cwd(), dir)) : [];
-
-  each(schemas, schema => {
-    ajv.addSchema(require(path.join(process.cwd(), dir, schema)), path.basename(schema, '.js'));
-  });
 };
